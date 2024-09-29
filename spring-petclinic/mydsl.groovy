b4abcf5f4bebe6697c6a410ce46aa49cfade1312
Shlomi Lory <shlomi.lory@mail.huji.ac.il>
2024-09-29 16:38:32 +0000

    DSL added

---
+++ spring-petclinic/mydsl.groovy
@@ -0,0 +1,51 @@
+pipeline {
+    agent any
+    
+    environment {
+        IMAGE = 'shlomilory/petclinic_proj'
+        GIT_CREDENTIALS_ID = 'GItc'
+        DOCKER_CREDENTIALS_ID = 'valhala'
+        
+    }
+    stages {
+        stage('Clone Repository') {
+            steps {
+                git credentialsId: "${GIT_CREDENTIALS_ID}", url: 'https://github.com/shlomilory/jb_project.git'
+            }
+        }
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    sh 'docker build -t ${IMAGE}:${BUILD_NUMBER} .'
+                }
+            }
+        }
+        stage('Tag Docker Image') {
+            steps {
+                script {
+                    sh 'docker tag ${IMAGE}:${BUILD_NUMBER} ${IMAGE}:latest'
+                }
+            }
+        }
+        stage('Push Docker Image to Docker Hub') {
+            steps {
+                script {
+                    withCredentials([usernamePassword(credentialsId: "${DOCKER_CREDENTIALS_ID}", usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+                        sh 'echo ${DOCKER_PASS} | docker login -u ${DOCKER_USER} --password-stdin'
+                    }
+                    
+                    sh 'docker push ${IMAGE}:${BUILD_NUMBER}'
+                    sh 'docker push ${IMAGE}:latest'
+                }
+            }
+        }
+    }
+    post {
+        success {
+            echo 'Docker image build and push successful!'
+        }
+        failure {
+            echo 'Docker image build or push failed.'
+        }
+    }
+}
