e049225563dbcca6934af2735a76ae2653195708
Shlomi Lory <shlomi.lory@mail.huji.ac.il>
2024-09-29 18:50:07 +0000

    now

--- spring-petclinic/mydsl.groovy
+++ spring-petclinic/mydsl.groovy
@@ -11,7 +11,7 @@ pipeline {
     stages {
         stage('Clone Repository') {
             steps {
-                git branch: 'main', url: 'https://github.com/shlomilory/jb_project.git', git credentialsId: "${GIT_CREDENTIALS_ID}"
+                git branch: 'main', url: 'https://github.com/shlomilory/jb_project.git'
             }
         }
         stage('Build Docker Image') {
